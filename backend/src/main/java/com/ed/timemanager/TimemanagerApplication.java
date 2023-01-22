b46668920ace66cf17f441bfec50e4252c2c963b
ed <edsim97@gmail.com>
2023-01-22 23:07:20 +0300

    смена версии

--- backend/src/main/java/com/ed/timemanager/TimemanagerApplication.java
+++ backend/src/main/java/com/ed/timemanager/TimemanagerApplication.java
@@ -11,13 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class TimemanagerApplication {
 
 	public static void main(String[] args) {
-		
+
 		SpringApplication.run(TimemanagerApplication.class, args);
 	}
 
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-
+	
 		return String.format("Hello %s!", name);
 	}
 }
